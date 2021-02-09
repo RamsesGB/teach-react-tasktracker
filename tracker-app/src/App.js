53f6b6ac642e584c64c60a5124dd7464e8f456ff
Ramses Bermudez <ramsesbermudez@Ramsess-MacBook-Pro.local>
2021-02-09 14:44:40 -0500

    Prop-drilling , map over array to display data

--- tracker-app/src/App.js
+++ tracker-app/src/App.js
@@ -1,10 +1,39 @@
 import './App.css';
+import { useState } from 'react';
 import Header from './Components/Header.js';
+import Tasks from './Components/Tasks.js';
 
 function App() {
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      text: 'Morning Pages',
+      day: 'Feb 8th @ 7:00AM',
+      reminder: true,
+    },
+    {
+      id: 2,
+      text: 'Gym',
+      day: 'Feb 9th @ 6:00AM',
+      reminder: false,
+    },
+    {
+      id: 3,
+      text: 'Meditate',
+      day: 'Feb 6th @ 9:00AM',
+      reminder: true,
+    },
+  ]);
+
+  // Delete Task
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id));
+  };
+
   return (
     <div className="container">
       <Header />
+      <Tasks tasks={tasks} onDelete={deleteTask} />
     </div>
   );
 }
--- tracker-app/src/Components/Header.js
+++ tracker-app/src/Components/Header.js
@@ -2,10 +2,14 @@ import Button from './Button';
 import PropTypes from 'prop-types';
 
 const Header = ({ title }) => {
+  const onClick = (e) => {
+    console.log(e);
+  };
+
   return (
     <header className="header">
       <h1>{title}</h1>
-      <Button color="green" text="Add" />
+      <Button onClick={onClick} color="green" text="Add" />
     </header>
   );
 };
